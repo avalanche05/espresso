02390ff87678e61bfaa76fa25e50c7e687b337a3
Mikhail <mihail.glazov2015@yandex.ru>
2021-12-29 15:38:42 +0300

    Initial commit

---
+++ MainWindow.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.gridLayout.addWidget(self.tableWidget, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
---
+++ main.py
@@ -0,0 +1,47 @@
+import sys
+import sqlite3
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QHeaderView, QApplication
+from MainWindow import Ui_MainWindow
+
+
+def get_items():
+    con = sqlite3.connect("coffee.sqlite")
+
+    cur = con.cursor()
+
+    result = cur.execute("""
+    SELECT * FROM coffee
+    """).fetchall()
+
+    print(len(result))
+
+    con.close()
+    return result
+
+
+class Main(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.update_result()
+
+    def update_result(self):
+        items = get_items()
+        self.tableWidget.verticalHeader().hide()
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.tableWidget.setRowCount(len(items))
+        parse_data = items
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setHorizontalHeaderLabels(
+            ['ID', 'название сорта', 'степень обжарки', 'молотый', 'описание вкуса', 'цена',
+             'объем упаковки'])
+        for i, elem in enumerate(parse_data):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = Main()
+    ex.show()
+    sys.exit(app.exec())
